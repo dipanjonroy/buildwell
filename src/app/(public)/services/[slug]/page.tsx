f62798167f9b3f8170baf696357ce0baea34534b
Dipanjon Chowdhury <dipanjon95@gmail.com>
2026-09-06 14:00:16 +0600

    feat:Hero Section added to Single Service page

--- src/app/(public)/services/[slug]/page.tsx
+++ src/app/(public)/services/[slug]/page.tsx
@@ -1,8 +1,8 @@
 import { services } from "@/libs/servicesData";
-import BookingButton from "@/components/ui/buttons/BookingButton";
 import { Metadata } from "next";
-import Image from "next/image";
 import { notFound } from "next/navigation";
+
+import HeroSection from "@/components/sections/HeroSection/SingleServiceHero";
 
 export async function generateMetadata({
   params,
@@ -18,7 +18,7 @@ export async function generateMetadata({
   if (!service) return notFound();
 
   return {
-    title: service?.heading,
+    title: service?.title,
     description: service?.text,
     openGraph: {
       images: [service?.img],
@@ -43,27 +43,12 @@ export default async function page({
 
   if (!service) return notFound();
   return (
-    <section className="w-full pt-50">
-      <div className="site-container">
-        <div className="space-y-20">
-          <div className="flex-col-center text-center gap-6">
-            <h1 className="heading font-bold text-6xl tracking-tight">{service.heading}</h1>
-            <p className="base-para w-full max-w-150">{service.text}</p>
-            <BookingButton variant="black"/>
-          </div>
-
-          <div className="relative w-full h-180 rounded-2xl overflow-hidden">
-            <Image
-              src={service.img}
-              alt={service.heading}
-              fill
-              sizes="100vw"
-              className="object-cover"
-              loading="eager"
-            />
-          </div>
-        </div>
-      </div>
-    </section>
+    <>
+      <HeroSection
+        title={service.title}
+        text={service.text}
+        img={service.img}
+      />
+    </>
   );
 }
