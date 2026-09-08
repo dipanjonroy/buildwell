3574c75e37efefaeaf6e56a8eb3c89ce161f0c19
Dipanjon Chowdhury <dipanjon95@gmail.com>
2026-09-08 15:00:18 +0600

    feat: Load more feature added to projects page

--- src/components/sections/AllProjectsSection/index.tsx
+++ src/components/sections/AllProjectsSection/index.tsx
@@ -3,18 +3,31 @@
 import { projectsData } from "@/libs/projectData";
 import { useState } from "react";
 import ProjectCard from "../PortfolioSection/ProjectCard";
+import PrimaryButton from "@/components/ui/buttons/PrimaryButton";
+
+const PROJECTS_PER_LOAD = 6;
 
 export default function AllProjects() {
+  const [visibleCount, setVisibleCount] = useState<number>(PROJECTS_PER_LOAD);
+
   const [activeTab, setActiveTab] = useState<string>("all");
 
-  const categories = [...new Set(projectsData.map((project)=>project.category))];
+  const categories = [
+    ...new Set(projectsData.map((project) => project.category)),
+  ];
 
   const filteredProjects =
     activeTab === "all"
       ? projectsData
       : projectsData.filter((project) => activeTab === project.category);
 
-  console.log(activeTab);
+ const visibleProjects = filteredProjects.slice(0, visibleCount);
+
+  const handleLoadMore = () => {
+    setVisibleCount((prev) => prev + PROJECTS_PER_LOAD);
+  };
+
+  const isMoreProjects = visibleCount < filteredProjects.length;
 
   return (
     <section className="section-padding">
@@ -22,7 +35,7 @@ export default function AllProjects() {
         <div className="w-full space-y-10">
           {/* Buttons */}
           <div className="flex-center">
-            <div className="helper-bg border black-border rounded-4xl px-8 py-4 black-text space-x-6">
+            <div className="flex-center flex-wrap helper-bg border black-border rounded-full px-8 py-4 black-text gap-x-6 gap-y-2">
               <button
                 className={`cursor-pointer font-bold ${activeTab === "all" ? "opacity-100" : "opacity-60"}`}
                 onClick={() => setActiveTab("all")}
@@ -41,11 +54,22 @@ export default function AllProjects() {
             </div>
           </div>
 
-          {/* Projects */}
-          <div className="grid grid-cols-2 gap-6">
-            {filteredProjects.map((project, idx) => (
-              <ProjectCard key={idx} details={project} featured={false} />
-            ))}
+          <div className="w-full flex-col-center gap-20">
+            {/* Projects */}
+            <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-8">
+              {visibleProjects.map((project, idx) => (
+                <ProjectCard key={idx} details={project} featured={false} />
+              ))}
+            </div>
+
+            {/* Load more button */}
+            {isMoreProjects && (
+              <PrimaryButton
+                btnName="Load More"
+                variant="black"
+                onClick={handleLoadMore}
+              />
+            )}
           </div>
         </div>
       </div>
