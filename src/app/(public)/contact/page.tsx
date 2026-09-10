5d2c752eaea8a7471a175d140cac9f9ef476c0e1
Dipanjon Chowdhury <dipanjon95@gmail.com>
2026-09-10 19:48:35 +0600

    Page: conact page added with hero section

---
+++ src/app/(public)/contact/page.tsx
@@ -0,0 +1,22 @@
+import HeroSection from "@/components/sections/CommonHero";
+import { Metadata } from "next";
+
+export const metadata:Metadata = {
+  title:"Conact us",
+  openGraph:{
+    images:["/contactpage/contact-hero.jpg"]
+  }
+}
+
+export default function page() {
+  return (
+    <>
+      <HeroSection
+        img="/contactpage/contact-hero.jpg"
+        badge="Contact Us"
+        headline="Let’s Build Something Great Together"
+        text="Have a project in mind or need help bringing your ideas to life? Get in touch with us today. We’d love to hear about your project and explore how we can help."
+      />
+    </>
+  );
+}
